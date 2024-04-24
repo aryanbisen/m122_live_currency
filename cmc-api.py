a095d679ad8d362d0ab812d3fc51207bc117e24a
aryanbisen <aryan.bisen@edu.tbz.ch>
2024-04-24 13:23:29 +0200

    add top five currencies function

--- cmc-api.py
+++ cmc-api.py
@@ -16,7 +16,14 @@ class CMC:
         data = r.json()['data']
         return data
 
-
+    def getTopCurrencies(self, limit=5):
+        url = f'{self.apiurl}/v1/cryptocurrency/listings/latest'
+        parameters = {'limit': limit}
+        r = self.session.get(url, params=parameters)
+        data = r.json()['data']
+        # Sort data based on market capitalization
+        sorted_data = sorted(data, key=lambda x: x['quote']['USD']['market_cap'], reverse=True)
+        return sorted_data
 
     def getPrice(self, symbol):
         url = self.apiurl + '/v2/cryptocurrency/quotes/latest'
@@ -31,4 +38,11 @@ cmc = CMC(APIKey.API_KEY)
 bitcoin_price = cmc.getPrice('BTC')
 print(f"The current price of Bitcoin is: ${bitcoin_price}")
 
-
+# Get top 5 currencies
+top_currencies = cmc.getTopCurrencies()
+print("\nTop 5 cryptocurrencies by market capitalization:")
+for i, currency in enumerate(top_currencies, start=1):
+    name = currency['name']
+    symbol = currency['symbol']
+    price = currency['quote']['USD']['price']
+    print(f"{i}. {name} ({symbol}): ${price}")
