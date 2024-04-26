49fa8a80a0baceedd0519a3ba8579eb462fb2120
KennethTBZ <kenneth.dang@edu.tbz.ch>
2024-04-26 13:23:13 +0200

    Implement top 5 currencies and improved UI.

--- code/app.py
+++ code/app.py
@@ -1,11 +1,16 @@
+# Import required modules
 from flask import Flask, render_template
 from requests import Session
 from dotenv import load_dotenv
 import os
 
+# Create Flask app instance
 app = Flask(__name__)
 
+# Load environment variables from .env file
 load_dotenv()
+
+# Define the CMC class
 class CMC:
     def __init__(self, token):
         self.apiurl = 'https://pro-api.coinmarketcap.com'
@@ -20,12 +25,27 @@ class CMC:
         data = r.json()
         return data
 
+    def getTopCurrencies(self, limit=5):
+        url = f'{self.apiurl}/v1/cryptocurrency/listings/latest'
+        parameters = {'limit': limit}
+        r = self.session.get(url, params=parameters)
+        data = r.json()['data']
+        
+        top_currencies = []
+        for currency in data[:limit]:
+            name = currency['name']
+            symbol = currency['symbol']
+            price = round(currency['quote']['USD']['price'], 2) 
+            top_currencies.append({'name': name, 'symbol': symbol, 'price': price})
+        return top_currencies
+
 cmc = CMC(os.getenv('API_KEY'))
 
 @app.route("/")
 def home():
-    btc_price = cmc.getPrice('BTC')['data']['BTC']['quote']['USD']['price']
-    return render_template("index.html", btc_price=btc_price)
+        btc_price = cmc.getPrice('BTC')['data']['BTC']['quote']['USD']['price']
+        top_currencies = cmc.getTopCurrencies()
+        return render_template("index.html", btc_price=btc_price, top_currencies=top_currencies)
 
 if __name__ == '__main__':
     app.run(debug=bool(os.getenv('DEBUG')), port=int(os.getenv('PORT')))
